84ada26afb15552503b68a3320d0979f17a2ebd3
Tiro Mosoeu <tmosoeu@c4r7s10.wethinkcode.co.za>
2020-06-17 15:38:02 +0200

    ex16 is done

---
+++ ex16/r_dwssap.sh
@@ -0,0 +1,14 @@
+export FT_LINE1=8
+export FT_LINE2=16    
+
+cat /etc/passwd | \
+    grep -v '\#' | \
+    sed '1!n;d' | \
+    cut -d':' -f1 | \
+    rev | \
+    sort -r | \
+    awk 'NR>= ENVIRON["FT_LINE1"] && NR<= ENVIRON["FT_LINE2"]' | \
+    paste -s -d"," - | \
+    sed 's/,/, /g' | \
+    sed 's/$/./' | \
+    tr -d '\n'
